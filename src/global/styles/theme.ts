863daa46ad8852d648a50808e8cc1aca9b5b3b5d
dpedersoli <dpmsengineer@gmail.com>
2023-12-11 22:51:21 -0300

    first commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,80 @@
+export const theme = {
+  color: {
+    primary400: "#C3EBF6",
+    primary300: "#200C49",
+    primary200: "#219EBF",
+    primary150: "#3B5BB1",
+    primary100: "#3C59B1",
+    primary50: "#5E87CC",
+    primary10: "#E5ECF7",
+
+    text: "#37474F",
+
+    black: "#121212",
+    gray500: "#3A3C3D",
+    gray400: "#4E4E4E",
+    gray300: "#838383",
+    gray200: "#B8B8B8",
+    gray100: "#EBEBEB",
+    gray50: "#FBFBFB",
+
+    red100: "#F7E5E5",
+    red200: "#FFC3C3",
+    red300: "#FF8E8E",
+
+    white: "#FFFFFF",
+
+    yellow100: "#F7F0E5",
+    yellow200: "#FFD98E",
+
+    weight0: "#00AE3A",
+    weight1: "#8BE500",
+    weight2: "#E7CD14",
+    weight3: "#FFB325",
+    weight4: "#FF431D",
+    weight100: "#5E87CC",
+
+    success: "#29B01D",
+    error: "#CD1919",
+    alert: "#FBC70F",
+
+    opaque: "rgba(32, 12, 73, 0.5)",
+  },
+
+  fontHeight: {
+    bold: 700,
+    semibold: 600,
+    regular: 400,
+  },
+
+  fonts: {
+    heading: "Roboto_700Bold",
+    body: "Roboto_400Regular",
+  },
+
+  borderRadius: {
+    extraSmall: 5,
+    small: 8,
+    medium: 16,
+    large: 24,
+    extraLarge: 40,
+  },
+
+  borderWidth: {
+    none: 0,
+    small: 1,
+    medium: 2,
+  },
+
+  opacity: {
+    low: 35,
+    high: 50,
+  },
+
+  shadow: {
+    x: 0,
+    y: 4,
+    b: 24,
+    opacity: 8,
+  },
+};
