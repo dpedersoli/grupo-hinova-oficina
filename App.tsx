2254c27d6195c33c2c2f7452253a81efe1ab73a0
dpedersoli <dpmsengineer@gmail.com>
2023-12-11 22:50:18 -0300

    feat: WIP Fonts & install typescript

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./src/global/styles/theme";
 
 export default function App() {
   return (
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: theme.fonts.heading,
   },
 });
