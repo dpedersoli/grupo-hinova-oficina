5e8fbbdb55dc9153155af4c63cf3e6e396312597
dpedersoli <dpmsengineer@gmail.com>
2023-12-14 19:18:09 -0300

    feat: add handleApiError

--- src/screens/AutoWorkshops/index.tsx
+++ src/screens/AutoWorkshops/index.tsx
@@ -1,20 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { ActivityIndicator, Alert, FlatList, Text, View } from "react-native";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Table } from "../../components/Table";
 import { theme } from "../../global/styles/theme";
 import { Workshops } from "../../interfaces/workshop.interface";
 import { api } from "../../services/api";
-import { AppError } from "../../utils/AppError";
 import { styles } from "./styles";
 
 export function AutoWorkshops() {
   const CODIGO_ASSOCIACAO = 601;
   const [workshops, setWorkshops] = useState<Workshops[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
 
   const navigation = useNavigation();
 
@@ -26,34 +24,20 @@ export function AutoWorkshops() {
     try {
       setIsLoading(true);
 
-      const response = await api.get(`/api/oficina`);
-      //?codigoAssociacao=${CODIGO_ASSOCIACAO}&cpfAssociado=
+      const response = await api.get(
+        `/api/oficina?codigoAssociacao=${CODIGO_ASSOCIACAO}&cpfAssociado=`
+      );
 
       setWorkshops(response.data.ListaOficinas);
     } catch (error) {
-      const isAppError = error instanceof AppError;
-
-      console.log("isAppError:", isAppError);
-
-      const title =
-        isAppError &&
-        "Não foi possível criar a conta. Tente novamente mais tarde";
-
-      setError(title);
-
-      // toast.show({
-      //   title,
-      //   placement: "top",
-      //   bgColor: "red.500",
-      // });
+      Alert.alert(
+        "Erro",
+        "Não foi possível carregar as oficinas. Tente novamente mais tarde"
+      );
     } finally {
       setIsLoading(false);
     }
   }
-
-  // {
-  //   error && Alert.alert("titulo", "error");
-  // }
 
   useEffect(() => {
     fetchWorkshops();
